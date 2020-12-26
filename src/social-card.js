ba2f7b323ac7c44fb3d1cc248a4a22059c80b957
Alex Hillman <alex@weknowhtml.com>
2020-12-26 17:41:35 -0500

    update for Indy Hall

--- src/social-card.js
+++ src/social-card.js
@@ -5,8 +5,8 @@ export default ({
   title = "Missing Title",
   width = "1024px",
   height = "512px",
-  author_avatar = "https://stackingthebricks.com/icon-256.png",
-  author_name = "Alex Hillman"
+  author_avatar = "https://new.indyhall.org/assets/images/indy-hall.png",
+  author_name = "Indy Hall"
 }) => {
   const styles = {
     width: width,
@@ -15,7 +15,6 @@ export default ({
   return (
     <div id="social-card" className="social-card" style={styles}>
       <div class="header"></div>
-      <img class="logo" alt="Stacking the Bricks" src="https://d33wubrfki0l68.cloudfront.net/cca6daea498584b465e8fc4d3ea004adf8676df1/31314/assets/stacking-the-bricks-logo.svg" width="200"/> 
       <h1>{title}</h1>
       <div class="author">
         <img src={author_avatar} alt={author_name} /> {author_name}
